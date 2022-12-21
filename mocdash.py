eaab9e479973512faeec8e97ada84429b93defe8
Mohamed Ibrahim <maxamedeid@gmail.com>
2022-12-21 12:28:32 +0300

    Update mocdash.py

--- mocdash.py
+++ mocdash.py
@@ -7,6 +7,7 @@ import pandas as pd
 df = pd.read_csv("data/mocdash.csv")
 fig = px.bar(df, x='Age', y='Likes')
 app = dash.Dash()
+server = app.server
 app.layout = html.Div([
     html.H1("Social Media Dashboard"),
     dcc.Graph(figure=fig)
