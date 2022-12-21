c830d43f70877a5e2cfdead63fa1550473a0ba39
Mohamed Ibrahim <maxamedeid@gmail.com>
2022-12-21 12:20:31 +0300

    Add files via upload

---
+++ mocdash.py
@@ -0,0 +1,15 @@
+import dash
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import pandas as pd
+
+#Import data 
+df = pd.read_csv("data/mocdash.csv")
+fig = px.bar(df, x='Age', y='Likes')
+app = dash.Dash()
+app.layout = html.Div([
+    html.H1("Social Media Dashboard"),
+    dcc.Graph(figure=fig)
+])
+
+app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
